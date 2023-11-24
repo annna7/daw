51b1680cc1ba52950006c884ce666dc569f27ee3
anna <annapecheanu1@gmail.com>
2023-11-24 20:43:53 +0200

    Laborator 7: am creat un proiect Angular si o componenta de tip counter

---
+++ Laborator7/counter-lab7/.angular/cache/17.0.9/vite/deps/@angular_platform-browser_animations.js
@@ -0,0 +1,21 @@
+import {
+  BrowserAnimationsModule,
+  InjectableAnimationEngine,
+  NoopAnimationsModule,
+  provideAnimations,
+  provideNoopAnimations
+} from "./chunk-V33C43PS.js";
+import "./chunk-K5ZOXLQC.js";
+import "./chunk-YVRZ3TUX.js";
+import {
+  ANIMATION_MODULE_TYPE
+} from "./chunk-PD7L26AE.js";
+export {
+  ANIMATION_MODULE_TYPE,
+  BrowserAnimationsModule,
+  NoopAnimationsModule,
+  provideAnimations,
+  provideNoopAnimations,
+  InjectableAnimationEngine as ɵInjectableAnimationEngine
+};
+//# sourceMappingURL=@angular_platform-browser_animations.js.map
